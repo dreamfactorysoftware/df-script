09f1c3838dcca14eff8e706f576c27aa44bd4836
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2018-12-31 11:15:21 +0200

    Add python3 scripting support

--- config/df.php
+++ config/df.php
@@ -8,6 +8,8 @@ return [
         'nodejs_path' => env('DF_NODEJS_PATH'),
         // path to the installed python executable
         'python_path' => env('DF_PYTHON_PATH'),
+        // path to the installed python3 executable
+        'python3_path' => env('DF_PYTHON3_PATH'),
         // protocol to use for Node.js and Python when making internal calls back to DreamFactory
         'default_protocol' => env('DF_SCRIPTING_DEFAULT_PROTOCOL', 'http'), // http or https
     ],
