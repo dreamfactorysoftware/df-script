ac9a0f979801d9706ba8c9ac84164ba588b3caa6
codyllord <codyllord@gmail.com>
2026-04-28 15:55:26 +0000

    fix method name for dispatchNow to dispatchSync due to method change from laravel version 9

--- src/Handlers/Events/ScriptableEventHandler.php
+++ src/Handlers/Events/ScriptableEventHandler.php
@@ -155,7 +155,7 @@ class ScriptableEventHandler
             }
         } elseif ($script = $this->getEventScript($event->name . '.queued')) {
             Log::debug('Queued service event script found: ' . $event->name);
-            $result = $this->dispatchNow(new ServiceEventScriptJob($event->name . '.queued', $event, $script->config));
+            $result = $this->dispatchSync(new ServiceEventScriptJob($event->name . '.queued', $event, $script->config));
             Log::debug('Service event queued: ' . $event->name . PHP_EOL . $result);
         }
 
